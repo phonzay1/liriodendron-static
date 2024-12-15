a03be0117f8e366d8600ea9aa8d01d3e89a2c84e
Ping Honzay <phonzay1@gmail.com>
2024-12-15 11:09:07 -0600

    Added tulipifera and resources pages

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 
 app.use(express.static('public'));
 
-app.listen(port, () => console.log(`Your awesome app is running on port ${port}!`));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/tulipifera', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/tulipifera.html'));
+});
+
+app.get('/resources', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/resources.html'));
+});
+
+app.listen(port, () => console.log(`Your app is running on port ${port}! :)`));
